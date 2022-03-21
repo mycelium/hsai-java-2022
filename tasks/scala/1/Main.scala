0635e8315be45ced796485f580cf8b60062981a6
Anton Lukashin <an.lukashin@gmail.com>
2022-03-21 10:49:39 +0300

    Change output for pascal triangle 10->16

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -4,7 +4,7 @@ import common._
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
-    for (row <- 0 to 10) {
+    for (row <- 0 to 16) {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
       println()
