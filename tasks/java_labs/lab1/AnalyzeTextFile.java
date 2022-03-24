19814f13b4c559934bb4c185acd108eadcc63f94
Mooth34 <astashoff.misha@gmail.com>
2022-03-24 20:04:55 +0500

    added implementations for file output

--- tasks/java_labs/lab1/AnalyzeTextFile.java
+++ tasks/java_labs/lab1/AnalyzeTextFile.java
@@ -2,50 +2,71 @@ package java_labs.lab1;
 
 import java.io.*;
 import java.nio.CharBuffer;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedList;
 
 public class AnalyzeTextFile {
     private static CharBuffer buffer;
     private static String[] words;
-    private static HashMap<Integer, Integer> words_lengths;
     private static String import_path = "", export_path = "";
-    private static boolean export_file;
+    private static boolean export_file = true;
 
-    private static int readFile(){
+    private static String readFile() {
         try (BufferedReader br = new BufferedReader(new FileReader(import_path))) {
             buffer = CharBuffer.allocate(((int) new File(import_path).length()));
-            return br.read(buffer);
+            return String.valueOf(br.read(buffer)) + '\n';
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(2);
         }
-        return -1;
+        return "\n";
     }
 
-    private static int countWords(){
+    private static String countWords() {
         words = String.valueOf(buffer.array()).split("([\\W\\s]+)");
-        return words.length;
+        return String.valueOf(words.length) + '\n';
     }
 
-    private static int countSpaces() {
+    private static String countSpaces() {
         int count = 0;
         for (char i : buffer.array())
-            if (i==' ')
+            if (i == ' ')
                 count++;
-        return count;
+        return String.valueOf(count) + '\n';
     }
 
     private static String countWordLengths() {
-        words_lengths = new HashMap<>();
-        for (String word : words){
+        HashMap<Integer, Integer> words_lengths = new HashMap<>();
+        for (String word : words) {
             if (!words_lengths.containsKey(word.length()))
                 words_lengths.put(word.length(), 1);
             else
-                words_lengths.put(word.length(), words_lengths.get(word.length())+1);
+                words_lengths.put(word.length(), words_lengths.get(word.length()) + 1);
         }
-        return words_lengths.toString();
+        return words_lengths.toString() + '\n';
+    }
+
+    private static void writeOutput() {
+        String output = readFile() + countWords() + countSpaces() + countWordLengths();
+
+        Writer writer;
+        if (export_file) {
+            try {
+                FileWriter fw = new FileWriter(export_path);
+                writer = new BufferedWriter(fw);
+            } catch (IOException e) {
+                System.out.println("Invalid export path!");
+                writer = new OutputStreamWriter(System.out);
+            }
+        } else
+            writer = new OutputStreamWriter(System.out);
+
+        BufferedWriter bw = new BufferedWriter(writer);
+        try {
+            bw.write(output);
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
@@ -53,22 +74,13 @@ public class AnalyzeTextFile {
             import_path = args[0];
             if (args.length > 1)
                 export_path = args[1];
-            export_file = false;
-        }
-        else {
+            else
+                export_file = false;
+        } else {
             System.out.println("Too few arguments!");
             System.exit(1);
         }
 
-        try {
-            FileWriter fw = new FileWriter(export_path);
-        } catch (IOException e) {
-
-            System.out.println("Invalid export path!");
-        }
-        System.out.println(readFile());
-        System.out.println(countWords());
-        System.out.println(countSpaces());
-        System.out.println(countWordLengths());
+        writeOutput();
     }
 }
