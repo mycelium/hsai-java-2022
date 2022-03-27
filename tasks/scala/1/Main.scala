0bf120aa4f9203084880df07596ceaeab2eb2b18
fogice-10 <98488649+fogice-10@users.noreply.github.com>
2022-03-27 15:51:12 +0300

    scala1 is done

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -9,20 +9,41 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+  /*  println("------------------------")
+    println("Exercise 2 Parentheses Balancing")
+    println(balance(")(".toList))
+    println(balance("())".toList))
+    println(balance("))())".toList))
+
+    println("------------------------")
+    println("Exercise 3 Counting Change")
+    println(countChange(10, List(5, 2, 3)))
+    println(countChange(10, List()))
+    println(countChange(0, List(50, 10)))*/
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r) {1}
+    else {
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def countParentheses(chars: List[Char], cnt: Int): Boolean = chars match {
+      case Nil => cnt == 0
+      case '(' :: tail => countParentheses(tail, cnt + 1)
+      case ')' :: tail => countParentheses(tail, cnt - 1)
+      case _ :: tail => countParentheses(tail, cnt)
+      }
+
+    countParentheses(chars, 0)
   }
 
   /**
@@ -33,6 +54,12 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money <= 0 || coins.isEmpty) {
+      if (money == 0) {1}
+      else {0}
+    }
+    else {
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    }
   }
 }
