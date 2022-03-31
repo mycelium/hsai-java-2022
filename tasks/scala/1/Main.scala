e14f4d28c0fe0876efc2d23e28a26fa35d31ce09
Mooth34 <astashoff.misha@gmail.com>
2022-03-31 11:59:26 +0500

    scala lab1 done

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -11,18 +11,38 @@ object Main {
     }
   }
 
+  println("\nParentheses Balancing")
+  println(balance("(())(())".toList))
+  println(balance("())(())".toList))
+  println(balance(")(())())".toList))
+  println(balance("".toList))
+
+  println("\nCounting Change")
+  println(countChange(5, List(2,3)))
+  println(countChange(11, List(1,5)))
+  println(countChange(7, List(1,2,3,5)))
+  println(countChange(0, List(1,2,3,5)))
+
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r) { 1 }
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def checkPattern(chars: List[Char], counter: Int): Boolean = chars match {
+      case '(' :: tail => checkPattern(tail, counter + 1)
+      case ')' :: tail => checkPattern(tail, counter - 1)
+      case _ :: tail => checkPattern(tail, counter)
+      case Nil => counter == 0
+    }
+
+    checkPattern(chars, 0)
   }
 
   /**
@@ -33,6 +53,17 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
+    def recursive(money: Int, coinsInner: List[Int]): Int = {
+      if (money <= 0) { if (money == 0) 1 else 0 }
+      else {
+        var counter = 0
+        for (i <- coinsInner.indices) {
+          counter += countChange(money - coinsInner(i), coinsInner.take(i + 1))
+        }
+        counter
+      }
+    }
 
+    recursive(money, coins.sorted)
   }
 }
