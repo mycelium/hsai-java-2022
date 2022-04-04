e38c6b17ff48fcad0673177ee34ac9fa08a15b96
Maksim Kan <frbeast@outlook.com>
2022-04-04 16:22:23 +0300

    Scala 1 - finished

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -2,6 +2,7 @@ package recfun
 
 object Main {
   def main(args: Array[String]): Unit = {
+    /* Подзадача 1 */
     // println("Pascal's Triangle")
     // for (row <- 0 to 10) {
     //   for (col <- 0 to row)
@@ -9,7 +10,11 @@ object Main {
     //   println()
     // }
 
-    print(balance("()()(()())".toList))
+    /* Подзадача 2 */
+    // print(balance("()()(()())".toList))
+
+    /* Подзадача 3 */
+    // print(countChange(19, List(1,2,5)))
   }
 
   /**
@@ -24,7 +29,7 @@ object Main {
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-    checkParentheses(chars.filter(c => (c != '(' || c != ')')), 0)
+    checkParentheses(chars.filter(c => (c == '(' || c == ')')), 0)
   }
 
   def checkParentheses(chars: List[Char], stack: Int): Boolean = {
@@ -39,14 +44,29 @@ object Main {
     else false
   }
 
-  //   /**
-  //    * Exercise 3 Counting Change
-  //    * Write a recursive function that counts how many different ways you can make
-  //    * change for an amount, given a list of coin denominations. For example,
-  //    * there is 1 way to give change for 5 if you have coins with denomiation
-  //    * 2 and 3: 2+3.
-  //    */
-  //   def countChange(money: Int, coins: List[Int]): Int = {
+  /**
+   * Exercise 3 Counting Change
+   * Write a recursive function that counts how many different ways you can make
+   * change for an amount, given a list of coin denominations. For example,
+   * there is 1 way to give change for 5 if you have coins with denomiation
+   * 2 and 3: 2+3.
+   */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    countChangeRecursive(money, coins.sorted(Ordering.Int.reverse))
+  }
 
-  //   }
+  def countChangeRecursive(money: Int, coins: List[Int]): Int = {
+    if (money == 0) {
+      1
+    }
+    else {
+      var count = 0
+      for (c <- coins) {
+        if (money >= c) {
+          count += countChange(money - c, coins.filter(_ <= c))
+        }
+      }
+      count
+    }
+  }
 }
