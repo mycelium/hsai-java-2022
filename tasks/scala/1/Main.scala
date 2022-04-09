00177ef33a1c51bcf129daa6b307d694e6ce0049
ematasova <katya-matasova@ya.ru>
2022-04-10 01:59:07 +0300

    scala 1 done

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,6 +1,3 @@
-package recfun
-import common._
-
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -15,14 +12,24 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if ((c == 0) || (c == r)) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def countBalance(chars: List[Char], acc: Int = 0): Int = {
+      chars match {
+        case Nil => acc
+        case '(' :: tail => countBalance(tail, acc + 1)
+        case ')' :: tail => if (acc > 0) countBalance(tail, acc - 1) else -1
+        case _ => countBalance(chars.tail, acc)
+      }
+    }
+
+    countBalance(chars) == 0
   }
 
   /**
@@ -33,6 +40,11 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
+    def countCombinations(money: Int, coins: List[Int]): Int =
+      if (money == 0) 1
+      else if (money < 0 || coins.isEmpty) 0
+      else countCombinations(money - coins.head, coins) + countCombinations(money, coins.tail)
 
+    countCombinations(money, coins)
   }
 }
