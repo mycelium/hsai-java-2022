f353ebe6c9d5966cec82e5f7c13b0558a1491c0a
LiliyaDCh <chelitsheva.Ld@edu.spbstu.ru>
2022-04-10 17:52:29 +0300

    Scala 1

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -14,14 +14,33 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if ((c == r) || (c == 0))
+      1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def count(chars: List[Char], num: Int): Boolean = {
+      if (chars.isEmpty)
+        num == 0
+      else {
+        val num2 = {
+          if (chars.head == '(')
+            num + 1
+          else if (chars.head == ')')
+            num - 1
+          else
+            num
+        }
+          if (num2 >= 0)
+            count(chars.tail, num2)
+          else false
+      }
+    }
+    count(chars, 0)
   }
 
   /**
@@ -32,6 +51,10 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money < 0 || coins.isEmpty)
+      0
+    else if (money == 0)
+      1
+    else countChange(money, coins.tail) + countChange(money - coins.head, coins)
   }
 }
