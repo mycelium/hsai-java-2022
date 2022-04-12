fa6d955b687c04358c28f94decf8f855b6716a34
Ekaterina Brovchenko <hameleon3301@mail.ru>
2022-04-13 00:09:25 +0300

    scala 1 done

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,28 +1,42 @@
-package recfun
 import common._
 
 object Main {
   def main(args: Array[String]) {
-    println("Pascal's Triangle")
+    println("Task 1")
     for (row <- 0 to 10) {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
       println()
     }
+
+//    println("Task 2")
+//    print(balance("((ds)())())".toList))
+
+//    println("Task 3")
+//    print(countChange(5, List(2,3)))
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == r || c == 0) return 1
+    pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    checkParentheses(chars.filter(c => c == '(' || c == ')'), 0)
+  }
+
+  def checkParentheses(chars: List[Char], count: Int): Boolean = {
+    if (chars.isEmpty) { count == 0 }
+    else if (count < 0) { false }
+    else if (chars.head == '(') { checkParentheses(chars.drop(1), count+1) }
+    else if (chars.head == ')') { checkParentheses(chars.drop(1), count-1) }
+    else false
   }
 
   /**
@@ -33,6 +47,13 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money == 0) { return 1 }
+    var count = 0
+    for (c <- coins) {
+      if (money >= c) {
+        count += countChange(money - c, coins.filter(_ <= c))
+      }
+    }
+    count
   }
 }
