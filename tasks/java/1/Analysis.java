dfe8a9cd26adc018957f23c37c25d6b8b4de4211
jacfly <98582294+jacfly@users.noreply.github.com>
2022-04-17 17:29:34 +0300

    Update Analysis.java

--- tasks/java/1/Analysis.java
+++ tasks/java/1/Analysis.java
@@ -1,12 +1,10 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Analysis {
     //Find words by splitting text
-    public static List<String> splitText(String s) {
-        String[] text_splitted = s.split("[\\s,!?.;:]+");
+    public static String[] splitText(String s) {
+        String[] text_splitted = s.split("[,\\s!?;:]+");
         List<String> words = new ArrayList<>();
         for (int i = 0; i < text_splitted.length; i++) {
             if (text_splitted[i].matches("[a-zA-Z]+"))
@@ -14,7 +12,7 @@ public class Analysis {
                 words.add(text_splitted[i]);
             }
         }
-        return words;
+        return words.toArray(new String[0]);
     }
 
     // Count spaces in text
@@ -32,34 +30,8 @@ public class Analysis {
     }
 
     // Create dictionary with word lengths as keys and number of words as values
-    public static Map<Integer, Integer> wordSizeMap(List<String> words) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-
-        for (int i = 0; i < words.size(); i++) {
-            int len = words.get(i).length();
-            if (map.containsKey(len)) {
-                map.put(len, map.get(len) + 1);
-            }
-            else {
-                map.put(len, 1);
-            }
-        }
-        return map;
-    }
-
-    // Create dictionary with symbols as keys and amount of appearance as values
-    public static Map<Character, Integer> symbolsMap(String text) {
-        Map<Character, Integer> map = new HashMap<Character, Integer>();
-        char[] textArray = text.toCharArray();
-
-        for (int i = 0; i < textArray.length; i++) {
-            if (map.containsKey(textArray[i])) {
-                map.put(textArray[i], map.get(textArray[i]) + 1);
-            }
-            else {
-                map.put(textArray[i], 1);
-            }
-        }
-        return map;
+    public static Map<Integer, Long> wordSizeMap(String[] words) {
+        return Arrays.stream(words)
+                .collect(Collectors.groupingBy(String::length, Collectors.counting()));
     }
 }
