5512974e4d59ed9cfb659aa48ba41c8e7040a629
fogice-10 <98488649+fogice-10@users.noreply.github.com>
2022-04-17 15:52:27 +0300

    Update Main.java

--- tasks/java/java1/Main.java
+++ tasks/java/java1/Main.java
@@ -1,73 +1,25 @@
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
-
-    public static void main(String args[]) {
-        // Using Scanner for Getting Input from User
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("Enter path to input file:");
-        String inputFile = in.nextLine();
-
-        System.out.println("Enter path to output file or print \"-\":");
-        String outputFile = in.nextLine();
-
-
-        try (
-                InputStream inputStream = new FileInputStream(inputFile);
-                Scanner scanner = new Scanner(inputStream, "Cp1252"); //ANSI
-
-                FileWriter fileWriter = new FileWriter(outputFile, true);
-        ) {
-                scanner.useDelimiter("\\Z");
-                String text = scanner.next();
-
-                // count words
-                int wordsNumber = TextAnalysis.countWordsUsingSplit(text);
-                // count spaces
-                int spaces = TextAnalysis.countSpaces(text);
-
-                // count word length with occurrence
-                String s = text.replaceAll("[.:;,#?!()/]","");
-                String[] words = s.split(" ");
-                List<String> targetList = Arrays.asList(words);
-                Map<Integer, Integer> wordSizeOccurance = TextAnalysis.countWordLengthOccurrence(targetList);
-
-                // count the occurrence of each character in a text
-                List<String> characterOccurrence = TextAnalysis.characterCount(text.toCharArray());
-
-                // output to console or write to file
-                if (outputFile.equals("-")) {
-                    System.out.println("Number of words:");
-                    System.out.println(wordsNumber);
-                    System.out.println("Number of spaces: ");
-                    System.out.println(spaces);
-                    System.out.println("Word length with occurrence: ");
-                    System.out.println(wordSizeOccurance);
-                    System.out.println("No. of each character: ");
-                    System.out.println(characterOccurrence);
-                }
-                else {
-                    BufferedWriter writer = new BufferedWriter(fileWriter);
-                    writer.write("Number of words: ");
-                    writer.write(String.valueOf(wordsNumber));
-                    writer.write(System.lineSeparator());
-                    writer.write("Number of spaces: ");
-                    writer.write(String.valueOf(spaces));
-                    writer.write(System.lineSeparator());
-                    writer.write("Word length with occurrence: ");
-                    writer.write(String.valueOf(wordSizeOccurance));
-                    writer.write(System.lineSeparator());
-                    writer.write("No. of each character: ");
-                    writer.write(String.valueOf(TextAnalysis.characterCount(text.toCharArray())));
-                    writer.close();
-                }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public  static  void  main (String[] args) throws IOException{
+        Path path = Path.of(args[0]);
+        String inputText = Files.readString(path);
+        if (!Files.exists(path)) {
+            System.out.println("File does not exist");
+            return;
         }
+
+        StringBuilder result = TextAnalysis.analyzeText(inputText);
+
+        if(args.length == 2){
+            Files.write(Path.of(args[1]), result.toString().getBytes(StandardCharsets.UTF_8));
+        }
+        else {
+            System.out.println(result);
+        }
+
     }
 }
