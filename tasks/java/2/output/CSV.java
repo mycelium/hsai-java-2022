fe6d9570f78bb2d6dfe29db24a10f53e917cdf73
jacfly <98582294+jacfly@users.noreply.github.com>
2022-04-20 13:01:15 +0300

    Update CSV.java

--- tasks/java/2/output/CSV.java
+++ tasks/java/2/output/CSV.java
@@ -4,20 +4,23 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.File;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CSV
 {
     public static void write(String path,  List<Double> numbers) throws Exception
     {
         File file = new File(path);
+
         FileWriter fw = new FileWriter(file);
         BufferedWriter bw = new BufferedWriter(fw);
 
-        for(int i = 0; i < numbers.size() - 1; i++) {
-            bw.write(numbers.get(i) + ",");
-        }
-        bw.write(numbers.get(numbers.size() - 1) + "");
+        String distributionLine = IntStream.range(0, numbers.size())
+                .mapToObj(i -> numbers.get(i) + "")
+                .collect(Collectors.joining(","));
 
+        bw.write(distributionLine);
         bw.close();
         fw.close();
     }
