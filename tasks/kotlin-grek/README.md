65c47985dab64b4c29167d778832af74ba122f5b
daniil.antokhin <a.d.ghost@mail.ru>
2022-04-20 14:00:35 +0300

    kotlin small fix

--- tasks/kotlin-grek/README.md
+++ tasks/kotlin-grek/README.md
@@ -1,20 +1,14 @@
-    Usage: app [OPTIONS] REGEX PATH
+    Usage: grek [OPTIONS] REGEX PATH
     
     Options:
-    -n, --line-number         Each output line is preceded by its relative line
-    number in the file, starting at line 1. The line
-    number counter is reset for each file processed.
-    -R, -r, --recursive       Recursively search subdirectories listed.
-    -A, --after-context INT   Print num lines of trailing context after each
-    match.
-    -B, --before-context INT  Print num lines of leading context before each
-    match.
-    -i, --ignore-case         Perform case insensitive matching. By default,
-    grek is case sensitive.
-    --exclude TEXT            If specified, it excludes files matching the given
-    filename.
+    -n, --line-number         Each output line is preceded by its relative line number in the file, starting at line 1. The line number counter is reset for each file processed
+    -R, -r, --recursive       Recursively search subdirectories listed
+    -A, --after-context INT   Print num lines of trailing context after each match
+    -B, --before-context INT  Print num lines of leading context before each match
+    -i, --ignore-case         Perform case insensitive matching. By default, grek is case sensitive
+    --exclude TEXT            If specified, it excludes files matching the given filename
     -h, --help                Show this message and exit
     
     Arguments:
-    REGEX  Regexp to match files.
-    PATH   Path to search in.
+    REGEX  Regexp to match files
+    PATH   Path to search in
--- tasks/kotlin-grek/src/main/kotlin/org/polytech/grek/Main.kt
+++ tasks/kotlin-grek/src/main/kotlin/org/polytech/grek/Main.kt
@@ -27,18 +27,17 @@ data class CmdParams(
 ) { }
 
 class App : CliktCommand() {
-    private val n: Boolean by option(names = arrayOf("-n", "--line-number"), help = "Each output line is preceded by its relative line number in the file, starting at line 1.  The line number counter is reset for each file processed.").flag()
-    private val r: Boolean by option(names = arrayOf("-R", "-r", "--recursive"), help = "Recursively search subdirectories listed.").flag()
-    private val a: Int by option(names = arrayOf("-A", "--after-context"), help = "Print num lines of trailing context after each match.").int().default(0)
-    private val b: Int by option(names = arrayOf("-B", "--before-context"), help = "Print num lines of leading context before each match.").int().default(0)
-    private val i: Boolean by option(names = arrayOf("-i", "--ignore-case"), help = "Perform case insensitive matching. By default, grek is case sensitive.").flag()
-    private val exclude: List<String> by option(names = arrayOf("--exclude"), help = "If specified, it excludes files matching the given filename.").multiple()
+    private val n: Boolean by option(names = arrayOf("-n", "--line-number"), help = "Each output line is preceded by its relative line number in the file, starting at line 1.  The line number counter is reset for each file processed").flag()
+    private val r: Boolean by option(names = arrayOf("-R", "-r", "--recursive"), help = "Recursively search subdirectories listed").flag()
+    private val a: Int by option(names = arrayOf("-A", "--after-context"), help = "Print num lines of trailing context after each match").int().default(0)
+    private val b: Int by option(names = arrayOf("-B", "--before-context"), help = "Print num lines of leading context before each match").int().default(0)
+    private val i: Boolean by option(names = arrayOf("-i", "--ignore-case"), help = "Perform case insensitive matching. By default, grek is case sensitive").flag()
+    private val exclude: List<String> by option(names = arrayOf("--exclude"), help = "If specified, it excludes files matching the given filename").multiple()
 
-    private val regex: String by argument(help = "Regexp to match files.")
-    private val path: Path by argument(help = "Path to search in.").path()
+    private val regex: String by argument(help = "Regexp to match files")
+    private val path: Path by argument(help = "Path to search in").path()
 
     override fun run() {
-        echo("a = $a, b = $b, n = $n, r = $r, i = $i, exclude = $exclude, regex = $regex, path = $path")
         val cmdParams = CmdParams(n, r, a, b, i, exclude, regex, path)
 
         try {
