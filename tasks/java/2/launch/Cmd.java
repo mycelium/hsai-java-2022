c0021a34351536ec6afecd8014fa3b3dde266723
jacfly <98582294+jacfly@users.noreply.github.com>
2022-04-21 01:26:24 +0300

    Update Cmd.java

--- tasks/java/2/launch/Cmd.java
+++ tasks/java/2/launch/Cmd.java
@@ -2,13 +2,23 @@ package launch;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.FileHandler;
 import java.util.stream.Collectors;
+import java.util.logging.Logger;
 
 import distributions.*;
 import output.CSV;
 
 public class Cmd {
+    private static Logger logs = Logger.getLogger("Logs");
+
     public static void main(String args[]) throws Exception {
+
+        // Write logs to file
+        FileHandler fh = new FileHandler("distribution.log");
+        logs.addHandler(fh);
+
+        // Fill arguments
         if (args.length == 0) {
             System.out.println("No arguments found");
             System.exit(-1);
@@ -30,6 +40,8 @@ public class Cmd {
 
         List<Double> num_list = null;
         System.out.println("Distribution's generation begins");
+        logs.info("Distribution's generation begins");
+
         switch (type) {
             case ("normal"):
                 Normal n = new Normal(size, params.get(0), params.get(1));
@@ -49,10 +61,15 @@ public class Cmd {
         }
 
         System.out.println("Distribution's generation is done");
+        logs.info("Distribution's generation is done");
+
+        // Write results to file
         CSV w = new CSV();
         String path = dir + "distribution.csv";
         w.write(path, num_list);
         System.out.println("Writing is done");
         System.out.println("Result saved in " + path);
+
+        logs.info("Distribution has been written to " + path);
     }
 }
