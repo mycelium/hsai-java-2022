0e45158aad66017e3edb698913e7682528ebd367
Yuri <ykhrishtakyan@gmail.com>
2022-04-20 13:07:12 +0300

    separate row count and keys -A -B with space work, minor cleanup

--- tasks/kotlin_task/src/main/kotlin/ArgsParser.kt
+++ tasks/kotlin_task/src/main/kotlin/ArgsParser.kt
@@ -1,35 +1,40 @@
 import kotlin.system.exitProcess
 
 fun parseArgs(args: Array<String>, settings: Arguments) {
-    for (arg in args) {
-        if (arg.contains("--help")) {
+    for (curr in args.indices) {
+        if (args[curr].contains("--help")) {
             showHelp()
             exitProcess(1)
         }
-        if (arg.contains("-i")) {
+        if (args[curr].contains("-i")) {
             settings.registerIgnore = true
             continue
         }
-        if (arg.contains("--exclude")) {
-            val files = arg.substring(arg.indexOf('=') + 1).split("|")
+        if (args[curr].contains("--exclude")) {
+            val files = args[curr].substring(args[curr].indexOf('=') + 1).split("|")
             settings.excludeList.addAll(files)
             continue
         }
-        if (arg.contains("-nr")) {
+        if (args[curr].contains("-nr")) {
             settings.directoryCheck = true
             continue
         }
-        if (arg.contains("-n")) {
+        if (args[curr].contains("-n")) {
             settings.fileCheck = true
             continue
         }
-        if (arg.contains("-B")) {
-            settings.beforeRows = arg.substring(arg.indexOf('B') + 1).toInt()
-            continue
+        try{
+            if (args[curr].contains("-B")) {
+                settings.beforeRows = args[curr + 1].toInt()
+                continue
+            }
+            if (args[curr].contains("-A")) {
+                settings.afterRows = args[curr + 1].toInt()
+                continue
+            }
         }
-        if (arg.contains("-A")) {
-            settings.afterRows = arg.substring(arg.indexOf('A') + 1).toInt()
-            continue
+        catch (e: Exception){
+            println("Wrong row count")
         }
     }
     settings.pattern = args[args.size - 2]
@@ -37,7 +42,7 @@ fun parseArgs(args: Array<String>, settings: Arguments) {
 }
 
 private fun showHelp() {
-    println("Example : -nr -i -A2 -B3 --exclude=data1|data2 wew \\testData")
+    println("Example : -nr -i -A 2 -B 3 --exclude=data1|data2 wew C:\\Users\\Forcause\\Downloads\\srcK\\ru\\testData")
     println("Keys: -n - single file\n-nr - directory\n-An - rows after (n - number of rows)\n-Bn - before rows (n - number of rows)")
     println("-i - ignore case\n--exclude - files to exclude in directory search\n--help - get help")
     println("Last two elements must be regex pattern and path")
--- tasks/kotlin_task/src/main/kotlin/Processor.kt
+++ tasks/kotlin_task/src/main/kotlin/Processor.kt
@@ -1,4 +1,3 @@
-import java.awt.Color
 import kotlin.system.exitProcess
 
 class Processor(reader: Reader) {
