9a7dfe232693b892afc44c9b8b54ce61a0835579
Ekaterina Basharina <kate290502@gmail.com>
2022-05-23 23:15:50 +0300

    Add scala1 homework

--- lessons/hsai-java-common/src/ru/spbstu/hsai/userservice/common/dto/GetUsersRequest.java
+++
@@ -1,7 +0,0 @@
-package ru.spbstu.hsai.userservice.common.dto;
-
-import java.io.Serializable;
-
-public class GetUsersRequest implements Serializable {
-    
-}
---
+++ tasks/scala/homework_scala1/Main.scala
@@ -0,0 +1,40 @@
+package homework_scala1
+
+object Main {
+  def main(args: Array[String]) = {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row) {
+        print(pascal(col, row) + " ")
+      }
+      println()
+    }
+
+    println(balance("()()".toList))
+    val coins = List(1, 5, 10);
+    print(countChange(17, coins));
+  }
+
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r) { return 1; }
+    else { return pascal(c - 1, r - 1) + pascal(c, r - 1); }
+  }
+
+  def balance(chars: List[Char]): Boolean = {
+    def calculate(chars: List[Char], counter: Int): Boolean = {
+      if (chars.isEmpty) { return counter == 0; }
+      if (chars.head == '(') { return calculate(chars.tail, counter + 1); }
+      if (chars.head == ')') { return calculate(chars.tail, counter - 1); }
+      else { return calculate(chars.tail, counter); }
+    }
+    return calculate(chars, 0);
+  }
+
+
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money < 0) { return 0; }
+    if (money == 0 || coins.length == 1) { return 1; }
+    return (countChange(money, coins.dropRight(1)) + countChange(money - coins.last, coins));
+  }
+
+}
