5676eae39403479b97335c32ee5522a619a2ff38
Alexandra <alexa.min31@gmail.com>
2022-05-24 18:39:58 +0300

    dependency fixes

--- tasks/java/2/io/src/main/java/module-info.java
+++ tasks/java/2/io/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module io {
     requires commons.cli;
     requires java.sql;
-    requires com.opencsv;
     exports spbstu.hsai.consoleGenerator.io.input;
     exports spbstu.hsai.consoleGenerator.io.output;
 }
