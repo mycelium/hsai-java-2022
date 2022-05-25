24723ff11e97e492a58da7a941314d296562ab2a
Skylight666 <skylight666@mail.ru>
2022-05-25 11:17:58 +0300

    The first scala lab

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,7 @@
 package recfun
-import common._
+
+//import common._
+import collection.mutable.Map
 
 object Main {
   def main(args: Array[String]) {
@@ -9,20 +11,35 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+    println(balance("[(([])]{})".toList))
+
+    val coins = List(1, 5, 10)
+    print(countChange(17, coins))
+//    print(countChangeDP(17, coins))
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == r | c == 0) {
+      1
+    } else {
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def balanced(chars: List[Char], open: Int): Boolean =
+      if (chars.isEmpty) open == 0
+      else if (chars.head == '(') balanced(chars.tail, open + 1)
+      else if (chars.head == ')') open > 0 && balanced(chars.tail, open - 1)
+      else balanced(chars.tail, open)
+    balanced(chars, 0)
   }
 
   /**
@@ -32,7 +49,30 @@ object Main {
    * there is 1 way to give change for 5 if you have coins with denomiation
    * 2 and 3: 2+3.
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
 
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money < 0 | coins.isEmpty) {
+      0
+    } else if (money == 0) {
+      1
+    } else {
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    }
+  }
+
+  def countChangeDP(money: Int, coins: List[Int]): Int = {
+    val combinations: Array[Int] = new Array[Int](money + 1)
+
+    combinations(0) = 1
+
+    for (coin <- coins) {
+      for (i <- coin to money) {
+        if (i >= coin) {
+          combinations(i) += combinations(i - coin)
+        }
+      }
+    }
+
+    combinations(money)
   }
 }
